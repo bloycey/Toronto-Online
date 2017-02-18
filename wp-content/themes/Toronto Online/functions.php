15e23864be8963970dce90e439a13b6377d0d4df
bloycey <bloyce1990@gmail.com>
2017-02-18 12:20:35 +1000

    Registered a widget space

--- wp-content/themes/Toronto Online/functions.php
+++ wp-content/themes/Toronto Online/functions.php
@@ -6,8 +6,30 @@ function torontoOnline_scripts() {
 
 add_action('wp_enqueue_scripts', 'torontoOnline_scripts');
 
+
+/**NAVIGATION**/
+
 register_nav_menus(array(
 			'main_menu' => __('Main Menu', 'torontoOnline')
 	) );
+
+
+/**Widget zone**/
+
+function theme_widgets() {
+    register_sidebar(array(
+    
+    'name'          => __('Sidebar Testimonials'),
+    'id'            => 'testimonials',
+    'description'   => 'Testimonials Widgets',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h3 class="widget_title">',
+    'after_title'   => '</h3>',
+    
+    ));
+}
+
+add_action('widgets_init', 'theme_widgets');
 
 add_filter('show_admin_bar', '__return_false');
