dab16b5813b76bee396a2f68952707f004803aa2
bloycey <bloyce1990@gmail.com>
2017-02-18 22:37:27 +1000

    Creating custom archive page

--- wp-content/themes/Toronto Online/page-what-visit.php
+++ wp-content/themes/Toronto Online/page-what-visit.php
@@ -22,7 +22,7 @@ get_header(); ?>
   <?php } ?>
   
   
-   <div id="primary" class="primary post-<?php the_ID(); ?>">
+   <div id="primary" class="primary no-sidebar post-<?php the_ID(); ?>">
  <?php  $args = array(
 
     'posts_per_page'    => 5,
@@ -32,13 +32,46 @@ get_header(); ?>
     ); ?>
     
     <?php $visit = new WP_Query($args); ?>
-    <?php while($visit->have_posts() ): $visit->the_post(); ?>
     
-    <?php print_r($visit);  ?>
+    <ul class="blog-visit">
+    
+    <?php while($visit->have_posts() ): $visit->the_post(); ?>
+    <li>
+        
+            <div class="featured">
+               <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('medium-blog'); ?>
+                </a>
+                
+                <?php the_category(', '); ?>
+            </div>
+        
+        
+            <div class="content">
+                
+                <h2><?php the_title(); ?></h2>
+                
+                <?php the_excerpt(); ?>
+                
+            </div>
+            
+            <div class="post-information">
+               
+                <div class="author">
+                     By: <?php the_author(); ?>
+                </div>
+                <div class="date">
+                    <?php the_time('F j, Y'); ?>
+                </div>
+                
+            </div>
+        
+        
+    </li>
+    
+    
     
     <?php endwhile; wp_reset_postdata(); ?>
 </div>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
